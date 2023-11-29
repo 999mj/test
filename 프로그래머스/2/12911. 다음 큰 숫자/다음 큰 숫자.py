50ed98d2f258ffbd5771d0373232a9304243634c
999mj <145695115+999mj@users.noreply.github.com>
2023-11-30 00:15:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,8 @@
+def solution(n):
+    cnt = bin(n).count('1')
+    for i in range(n+1,1000001):
+        i_count=bin(i).count('1')
+        if cnt == i_count:
+            answer = i
+            break
+    return answer
